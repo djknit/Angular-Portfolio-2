6d383ab2a4252e70a6f8c17b927bb4664882c798
David <djknit@gmail.com>
2019-08-16 20:33:58 -0500

    began adding about section animations

--- app/components/about-section/about.component.js
+++ app/components/about-section/about.component.js
@@ -3,5 +3,27 @@
 angular
   .module('aboutSection')
   .component('aboutSection', {
-    templateUrl: 'components/about-section/about.template.html'
+    templateUrl: 'components/about-section/about.template.html',
+    controller: function aboutSectionCtrl() {
+      this.isExpanded = null;
+
+      this.aboutBasicStyle = function() {
+        return {
+          get height() {
+            if (this.isExpanded === null) return 'auto';
+            return this.isExpanded ? this.tallHeight : this.shortHeight;
+          }
+        }
+      };
+
+      this.setHeights = function() {
+        this.shortHeight = $('#about-basic > h2').outerHeight(true) + $('#about-basic > p:first-of-type').outerHeight(true);
+        this.tallHeight = this.shortHeight + $('#about-basic > p:last-child').outerHeight(true);
+      }
+
+      this.aboutBasicHeight = function() {
+        if (this.isExpanded === null) return 'auto';
+        return this.isExpanded ? this.tallHeight : this.shortHeight;
+      }
+    }
   });
