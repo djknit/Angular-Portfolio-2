71fe2a66d79cc0ece300741f64be2095c9ab167b
David <djknit@gmail.com>
2019-08-18 17:29:35 -0500

    got about section animations working

--- app/components/about-section/about.component.js
+++ app/components/about-section/about.component.js
@@ -4,26 +4,83 @@ angular
   .module('aboutSection')
   .component('aboutSection', {
     templateUrl: 'components/about-section/about.template.html',
-    controller: function aboutSectionCtrl() {
+    controller: ['$timeout', '$window', '$scope', function aboutSectionCtrl($timeout, $window, $scope) {
       this.isExpanded = null;
+      this.lastPHeight = null;
+      this.hideLastP = true;
+      this.toggleButtonText = 'Show More'
+      this.buttonTextFade = '';
+      this.iconFlip = '';
 
-      this.aboutBasicStyle = function() {
+      this.openOrClosed = function() {
+        if (this.isExpanded === null) return '';
+        else return this.isExpanded ? 'open' : 'closed';
+      }
+
+      this.lastPStyle = function() {
+        const isHeightSet = this.lastPHeight !== null;
         return {
-          get height() {
-            if (this.isExpanded === null) return 'auto';
-            return this.isExpanded ? this.tallHeight : this.shortHeight;
-          }
+          height: isHeightSet ? this.lastPHeight + 'px' : 'auto',
+          position: this.hideLastP ? 'absolute' : 'static',
+          opacity: this.hideLastP ? 0 : 1,
+          marginBottom: 0
         }
-      };
-
-      this.setHeights = function() {
-        this.shortHeight = $('#about-basic > h2').outerHeight(true) + $('#about-basic > p:first-of-type').outerHeight(true);
-        this.tallHeight = this.shortHeight + $('#about-basic > p:last-child').outerHeight(true);
       }
 
-      this.aboutBasicHeight = function() {
-        if (this.isExpanded === null) return 'auto';
-        return this.isExpanded ? this.tallHeight : this.shortHeight;
+      this.toggleButtonClass = function() {
+        return this.openOrClosed() + ' ' + this.buttonTextFade;
       }
-    }
+
+      function setHeight() {
+        this.lastPHeight = $('#about-basic > p:last-child').innerHeight();
+      }
+
+      this.setHeight = setHeight.bind(this);
+
+      this.setHeight();
+
+      // source: https://stackoverflow.com/questions/31622673/angularjs-watch-window-resize-inside-directive
+      let screenWidth = $window.innerWidth;
+      angular.element($window).bind('resize', () => {
+        const newWidth = $window.innerWidth;
+        if (newWidth !== screenWidth) {
+          screenWidth = newWidth;
+          this.lastPHeight = null;
+          if (!this.isExpanded) {
+            this.hideLastP = true;
+            this.isExpanded = null;
+          }
+          $scope.$apply();
+        }
+      });
+
+
+      this.toggleAboutSection = function() {
+        if (this.lastPHeight === null) this.setHeight();
+        if (this.isExpanded) {
+          this.iconFlip = 'flip-down';
+          const self = this;
+        }
+        else {
+          this.iconFlip = 'flip-up';
+        }
+        this.isExpanded = !this.isExpanded;
+        this.hideLastP = false;
+        this.buttonTextFade = 'fade-out';
+        const self = this;
+        $timeout(this.changeButtonText, 1000);
+      }
+
+      function changeButtonText() {
+        console.log(this)
+        if (this.isExpanded) {
+          this.toggleButtonText = 'Show Less';
+        }
+        else {
+          this.toggleButtonText = 'Show More';
+        }
+        this.buttonTextFade = 'fade-in';
+      }
+      this.changeButtonText = changeButtonText.bind(this);
+    }]
   });
